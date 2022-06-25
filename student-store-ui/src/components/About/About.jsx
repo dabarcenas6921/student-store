2ca6a05d35e3d4c3545b0c6b30a24020ed6b8011
David Barcenas <dbarcenas@dbarcen-ltmc97g.internal.salesforce.com>
2022-06-25 16:29:11 -0700

    did css for the about page

--- student-store-ui/src/components/About/About.jsx
+++ student-store-ui/src/components/About/About.jsx
@@ -4,9 +4,34 @@ import "./About.css";
 export default function About() {
   return (
     <div className="about-header">
-      <a id="About">
-        <h1>About</h1>
-      </a>
+      <div class="about-content">
+        <a id="About">
+          <h1>About</h1>
+        </a>
+        <div class="about-summary">
+          <div class="about-text">
+            <p>
+              The codepath student store offers great products at great prices
+              from a great team and for a great cause.
+            </p>
+            <p>
+              We've searched far and wide for items that perk the interests of
+              even the most eccentric students and decided to offer them all
+              here in one place.
+            </p>
+            <p>
+              All proceeds go towards bringing high quality CS education to
+              college students around the country.
+            </p>
+          </div>
+          <div class="about-media">
+            <img
+              src="https://codepath-student-store-demo.surge.sh/assets/giant_codepath.6952ef57.svg"
+              alt="codepath large"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
